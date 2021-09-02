aefa5c65ed1a661b8a55d4d67822d98b8df2e6a5
tobor <rosborne@osbornepro.com>
2021-09-02 16:32:12 -0600

    Update Update-Elasticsearch.ps1
    Updated my help comments

--- Update-Elasticsearch.ps1
+++ Update-Elasticsearch.ps1
@@ -3,9 +3,9 @@
 # Script I am working on to automatically update Elasticsearch on a Windows Server
 #
 # For this to work you will need to modify lines 8, 21, 22, 27, 28, 33, 34, 63, 64
-# Baseed on how you set things up you may or may not need to modify lines 14, 19, 23, 25, 31, 74
+# Baseed on how you set things up you may or may not need to modify lines 14, 19, 23, 25, 31, 44, 45, 74, 75, 87
 
-$CurrentVersion = (.'C:\elk\elasticsearch\bin\elasticsearch.bat' --version 2> $Null).Split(' ')[1].Replace(',','')
+$CurrentVersion = (.'C:\Program Files\elasticsearch\bin\elasticsearch.bat' --version 2> $Null).Split(' ')[1].Replace(',','')
 #$Response = Invoke-WebRequest -Method GET -Uri "https://www.elastic.co/downloads/elasticsearch" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
 #$NewVersion = $Response.ParsedHtml NOT FINISHED
 $Version = Read-Host -Prompt "What is the latest version of Elasticsearch available. EXAMPLE: 7.14.0"
@@ -19,19 +19,19 @@ $ELKHashTable.Elasticsearch = @()
 $ELKHashTable.Elasticsearch += "elasticsearch"
 $ELKHashTable.Elasticsearch += "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$Version-windows-x86_64.zip"
 $ELKHashTable.Elasticsearch += "C:\Program Files\elasticsearch"
-$ELKHashTable.Elasticsearch += "C:\Program Files\elasticsearch\config\elasticsearch.yml"
+$ELKHashTable.Elasticsearch += "C:\Program Files\elasticsearch.bak\config\elasticsearch.yml"
 $ELKHashTable.Elasticsearch += "elasticsearch-service-x64"
 $ELKHashTable.Kibana = @()
 $ELKHashTable.Kibana += "kibana"
 $ELKHashTable.Kibana += "https://artifacts.elastic.co/downloads/kibana/kibana-$Version-windows-x86_64.zip"
 $ELKHashTable.Kibana += "C:\Program Files\kibana"
-$ELKHashTable.Kibana += "C:\Program Files\kibana\config\kibana.yml"
+$ELKHashTable.Kibana += "C:\Program Files\kibana.bak\config\kibana.yml"
 $ELKHashTable.Kibana += "kibana"
 $ELKHashTable.Winlogbeat = @()
 $ELKHashTable.Winlogbeat += "winlogbeat"
 $ELKHashTable.Winlogbeat += "https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-$Version-windows-x86_64.zip"
 $ELKHashTable.Winlogbeat += "C:\Program Files\winlogbeat"
-$ELKHashTable.Winlogbeat += "C:\Program Files\winlogbeat\winlogbeat.yml"
+$ELKHashTable.Winlogbeat += "C:\Program Files\winlogbeat.bak\winlogbeat.yml"
 $ELKHashTable.Winlogbeat += "winlogbeat"
 
 ForEach ($Elk in $ELKHashTable.Keys) {
